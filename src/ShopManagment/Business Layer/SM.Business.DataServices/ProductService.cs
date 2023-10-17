fb1f281f74102e579f84c1446b7b02ab61cf8ac5
umar-source <132484863+umar-source@users.noreply.github.com>
2023-10-17 20:13:33 +0500

    Clean Architecture

---
+++ src/ShopManagment/Business Layer/SM.Business.DataServices/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using SM.Business.Models;
+
+namespace SM.Business.DataServices
+{
+    public class ProductService
+    {
+      
+
+
+        public List<ProductModel>  GetAll()
+        {
+            List<ProductModel> products = new List<ProductModel>
+            {
+                new ProductModel { Id = 1, Name = "asdv", Description = "igofjdenkfdbv" },
+                new ProductModel { Id = 2, Name = "asdv", Description = "igofjdenkfdbv" },
+                new ProductModel { Id = 3, Name = "asdv", Description = "igofjdenkfdbv" },
+                new ProductModel { Id = 4, Name = "asdv", Description = "igofjdenkfdbv" },
+                new ProductModel { Id = 6, Name = "asdv", Description = "igofjdenkfdbv" },
+                new ProductModel { Id = 5, Name = "asdv", Description = "igofjdenkfdbv" }
+            };
+
+            return products;
+        }
+
+     }
+}
---
+++ src/ShopManagment/UI Layer/SM.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,96 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using SM.Business.DataServices;
+using SM.Business.Models;
+
+namespace SM.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+
+            _productService = productService;
+        }
+
+
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            List<ProductModel> productList = _productService.GetAll(); // Replace with your actual data retrieval logic
+            return View(productList);
+        }
+
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
